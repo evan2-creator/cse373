48b0674f17078162d7de32d9b43d597361408f03
Yingfan Chen <yingfc@uw.edu>
2020-05-05 13:35:22 +0800

    ArrayHeapMinPQ - Completed version 1.0

--- heap/src/priorityqueues/ArrayHeapMinPQ.java
+++ heap/src/priorityqueues/ArrayHeapMinPQ.java
@@ -1,18 +1,23 @@
 package priorityqueues;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class ArrayHeapMinPQ<T extends Comparable<T>> implements ExtrinsicMinPQ<T> {
     // IMPORTANT: Do not rename these fields or change their visibility.
     // We access these during grading to test your code.
-    static final int START_INDEX = 1;
+    static final int START_INDEX = 0;
     List<PriorityNode<T>> items;
-    // TODO: add fields as necessary
+
+    private int size;
+    private HashMap<T, Integer> itemsMap;
 
     public ArrayHeapMinPQ() {
         items = new ArrayList<>();
-        // TODO: add code as necessary
+        itemsMap = new HashMap<>();
+        size = 0;
     }
 
     // Here's a method stub that may be useful. Feel free to change or remove it, if you wish.
@@ -21,43 +26,116 @@ public class ArrayHeapMinPQ<T extends Comparable<T>> implements ExtrinsicMinPQ<T
      * A helper method for swapping the items at two indices of the array heap.
      */
     private void swap(int a, int b) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        PriorityNode<T> node1 = items.get(a);
+        PriorityNode<T> node2 = items.get(b);
+        items.set(a, node2);
+        items.set(b, node1);
+        itemsMap.put(node1.getItem(), b);
+        itemsMap.put(node2.getItem(), a);
     }
 
     @Override
     public void add(T item, double priority) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        if (contains(item) || item == null) {
+            throw new IllegalArgumentException();
+        } else {
+            items.add(new PriorityNode<>(item, priority));
+            itemsMap.put(item, size);
+            swimUp(size);
+            size++;
+        }
     }
 
     @Override
     public boolean contains(T item) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return itemsMap.containsKey(item);
     }
 
     @Override
     public T peekMin() {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        if (size() > START_INDEX) {
+            return items.get(START_INDEX).getItem();
+        } else {
+            throw new NoSuchElementException();
+        }
     }
 
     @Override
     public T removeMin() {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        if (size() > START_INDEX) {
+            T removed = peekMin();
+            swap(START_INDEX, size - 1);
+            itemsMap.remove(removed);
+            size--;
+            swimDown(START_INDEX);
+            return removed;
+        } else {
+            throw new NoSuchElementException();
+        }
     }
 
     @Override
     public void changePriority(T item, double priority) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        if (contains(item)) {
+            int index = itemsMap.get(item);
+            if (priority < items.get(index).getPriority()) {
+                items.get(index).setPriority(priority);
+                swimUp(index);
+            } else {
+                items.get(index).setPriority(priority);
+                swimDown(index);
+            }
+        } else {
+            throw new NoSuchElementException();
+        }
+
     }
 
     @Override
     public int size() {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return size;
+    }
+
+    private int parentIndex(int childIndex) {
+        return (childIndex - 1) / 2;
+    }
+
+    private int leftIndex(int parentIndex) {
+        return parentIndex * 2 + 1;
+    }
+
+    private int rightIndex(int parentIndex) {
+        return parentIndex * 2 + 2;
+    }
+
+    private void swimUp(int index) {
+        if (index > START_INDEX) {
+            int parent = parentIndex(index);
+            if (items.get(index).getPriority() < items.get(parent).getPriority()) {
+                swap(index, parent);
+                swimUp(parent);
+            }
+        }
+    }
+
+    private void swimDown(int index) {
+        int sinkIndex = findSmaller(index);
+        while (items.get(index).getPriority() > items.get(sinkIndex).getPriority()) {
+            swap(index, sinkIndex);
+            index = sinkIndex;
+            sinkIndex = findSmaller(index);
+        }
+    }
+
+    private int findSmaller(int parentIndex) {
+        int left = leftIndex(parentIndex);
+        int right = rightIndex(parentIndex);
+        if (left < size() && items.get(left).getPriority() < items.get(parentIndex).getPriority()) {
+            parentIndex = left;
+        }
+        if (right < size() && items.get(right).getPriority() < items.get(parentIndex).getPriority()) {
+            parentIndex = right;
+        }
+        return parentIndex;
     }
 }
