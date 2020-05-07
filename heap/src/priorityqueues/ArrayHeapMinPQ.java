903a0a6ba8abd0862d3a4c214371d4b326ec6f7b
Haonan Xu <haruhi@uw.edu>
2020-05-06 22:48:53 -0700

    commit

--- heap/src/priorityqueues/ArrayHeapMinPQ.java
+++ heap/src/priorityqueues/ArrayHeapMinPQ.java
@@ -120,10 +120,14 @@ public class ArrayHeapMinPQ<T extends Comparable<T>> implements ExtrinsicMinPQ<T
 
     private void swimDown(int index) {
         int sinkIndex = findSmaller(index);
-        while (items.get(index).getPriority() > items.get(sinkIndex).getPriority()) {
+        // while (items.get(index).getPriority() > items.get(sinkIndex).getPriority()) {
+        //     swap(index, sinkIndex);
+        //     index = sinkIndex;
+        //     sinkIndex = findSmaller(index);
+        // }
+        if (sinkIndex > index) {
             swap(index, sinkIndex);
-            index = sinkIndex;
-            sinkIndex = findSmaller(index);
+            swimDown(sinkIndex);
         }
     }
 
